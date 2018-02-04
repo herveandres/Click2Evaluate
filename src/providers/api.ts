0b22cc95e87125f777f5769f8e0d78d5e573463b
HerrVey <handres670@gmail.com>
2018-02-04 21:00:48 +0100

    Rebase

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -1,6 +1,6 @@
 // This class contains all data used to connect the API
 
 export class API{
-  url: string = "http://127.0.0.1:8000/api/";
-  noServer: boolean = true;
+  url: string = "http://149.202.63.91:8000/api/";
+  noServer: boolean = false;
 }
